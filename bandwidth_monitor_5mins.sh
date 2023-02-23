daf2165d0cdb3ae5b7c82b7d340c9e96be66df9a
John Chen <tetlun54@gmail.com>
2023-02-23 20:57:14 +0800

    Updates

--- bandwidth_monitor_5mins.sh
+++ bandwidth_monitor_5mins.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Define the network interface to monitor
 NETWORK_INTERFACE="p2p1"
 INTERFACE_SPEED=10000000000 # 10 Gbps
-
-# Define the list of process names to monitor
 PROCESS_NAMES=("dcache" "css")
 
-# Initialize flag variable to indicate if any processes were found
+INTERVAL = 300
+
+DIR = "/root/bandwidth_calc_tc"
+DAILY_DIR = "${DIR}/daily"
+MONTHLY_DIR = "${DIR}/monthly"
+
 found_process=false
 
-# Check if the network interface exists
+# Check if the network interface exists, else exit
 ip link show $NETWORK_INTERFACE >/dev/null 2>&1 || { echo >&2 "Error: Network interface $NETWORK_INTERFACE does not exist. Aborting."; exit 1; }
 
 # Loop through each process name and set up tc and net_cls rules
@@ -25,12 +27,12 @@ do
     # Generate a unique CLASS_ID for the process
     CLASS_ID="$(uuidgen)"
 
-    # Set up tc qdisc and class for the process
+    # Set up tc qdisc 
     sudo tc qdisc add dev $NETWORK_INTERFACE root handle 1: htb default 12
     sudo tc class add dev $NETWORK_INTERFACE parent 1: classid 1:$CLASS_ID htb rate ${INTERFACE_SPEED}bps
     sudo tc filter add dev $NETWORK_INTERFACE parent 1: prio 2 protocol ip handle $CLASS_ID fw classid 1:$CLASS_ID
 
-    # Set up net_cls cgroup for the process
+    # Set up net_cls cgroup
     sudo cgcreate -g net_cls:$CLASS_ID
     sudo cgset -r net_cls.classid=$CLASS_ID $CLASS_ID
 
@@ -44,12 +46,12 @@ do
   fi
 done
 
-# If >= 1 process available, wait for 5 minutes
+# If >= 1 process available, wait for 5 minutes, else exit
 if [ "$found_process" = false ]; then
   echo "No ${PROCESS_NAMES[@]} found. Exiting script."
   exit 1
 else
-    sleep 300
+    sleep $INTERVAL
 fi
 
 # Generate a daily report of the total bandwidth sent by each process
@@ -60,7 +62,7 @@ do
 
   if [ -n "$pid" ]; then
     # Generate the file name for the daily report
-    report_file="bandwidth_daily_report_$process_name.log"
+    report_file="${DAILY_DIR}/bandwidth_daily_report_$process_name.log"
 
     # Get the total number of bytes transmitted by the process
     BYTES_SENT="$(sudo tc -s -d class show dev $NETWORK_INTERFACE classid 1:$CLASS_ID | awk '/bytes/ {print $2}')"
